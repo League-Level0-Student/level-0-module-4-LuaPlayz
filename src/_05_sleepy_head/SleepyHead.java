037227ff071e40d87b23da64866165e46af5bd8f
root <root@d26b718cc882>
2020-03-26 01:30:09 +0000

    level 0 mod 4

--- src/_05_sleepy_head/SleepyHead.java
+++ src/_05_sleepy_head/SleepyHead.java
@@ -18,8 +18,25 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
+    int weekday=JOptionPane.showConfirmDialog(null, "IS IT A WEEKDAY?", "SLEEPY HEAD",JOptionPane.YES_NO_OPTION);
+if (weekday==0) {
+	isWeekday=true;
+}
+else {
+	isWeekday=false;
+}
+	int vacation=JOptionPane.showConfirmDialog(null, "IS IT A VACATION MY CHILD???","IS IT VACATION", JOptionPane.YES_NO_OPTION);
+if (weekday==0) {
+	isVacation=true;
+}else {
+	isVacation=false;
+}
 
+if(isVacation==true || isWeekday==false) {
+	System.out.println("SLEEEP INNN ALLL YOUUU WANTTT MYYYY CHILDDD!!!");
+}else {
+	System.out.println("GET UP LAZY BONES!!!");
+}
 
         /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
