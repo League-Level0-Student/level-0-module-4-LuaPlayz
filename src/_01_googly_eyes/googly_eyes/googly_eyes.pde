96bfee02d0b06f917a2dcb8e35d004e08c2bad30
root <root@e179dd190a74>
2020-04-16 01:23:16 +0000

    hi

--- src/_01_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_01_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,6 +1,8 @@
-
+PImage face;
 void setup() {
-  
+  face=loadImage("dog.jpg");
+  size(800,600);
+  face.resize(800,600);
 }
 
 void draw() {
--- src/_08_happy_pet/HappyPet.java
+++ src/_08_happy_pet/HappyPet.java
@@ -13,6 +13,8 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String pet=JOptionPane.showInputDialog(null, "WHAT KIND OF PET DO YOU WANT MY CHILD??? A DOGGO OR A KITTY???");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 11; i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
@@ -21,19 +23,38 @@ String pet=JOptionPane.showInputDialog(null, "WHAT KIND OF PET DO YOU WANT MY CH
 					new String[] {"water", "feed"}, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if (task==0) {
+	water(pet);
+}
+if (task==1) {
+	feed(pet);
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if (happinessLevel==10) {
+	break;
+}
+	}
 	}
 
-	// 4. Create methods to handle each of your user selections.
+	static // 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 	void water(String pet) {
 		if (pet.equals("kitty")) {
 			JOptionPane.showMessageDialog(null,"the cat purrrrs");
 		happinessLevel++;
-		
+	}else {
+		JOptionPane.showMessageDialog(null, "The doggo wags its tail");
 	}
+	}
+	static void feed(String pet){
+		if(pet.equals("doggo")) {
+			JOptionPane.showMessageDialog(null, "The doggo sprints to the food and gobbles it down");
+		}else {
+			JOptionPane.showMessageDialog(null, "The cat licks you and eats its food");
+		}
+	
+	
+		}
 }
